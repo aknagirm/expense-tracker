51b787b0478d5bb88f94b521650c4957cf8c3a41
aknagirm <mriganka.s.sarkar@gmail.com>
2022-04-11 00:53:15 +0530

    logout added

--- Client/src/app/core/add-details/add-details.component.ts
+++ Client/src/app/core/add-details/add-details.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-details',
   templateUrl: './add-details.component.html',
-  styleUrls: ['./add-details.component.scss']
+  styleUrls: ['./add-details.component.scss'],
 })
 export class AddDetailsComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  logout() {
+    let userToken = localStorage.getItem('token');
+    if (userToken) {
+      localStorage.removeItem('token');
+      this.router.navigate(['login']);
+    }
   }
-
 }
